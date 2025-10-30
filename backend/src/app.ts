6806825b4fde7816e38b7a32b5563c515879cc47
Wille1989 <W.Lundqvist@live.se>
2025-10-30 19:32:04 +0100

    sparar

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:4173'],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
